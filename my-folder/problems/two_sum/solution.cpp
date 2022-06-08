4532f0d3de07548b1ca094bcda137fe61542d788
Jakub Kowalczyk <36115121+Fangolinn@users.noreply.github.com>
2022-06-08 08:10:54 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- my-folder/problems/two_sum/solution.cpp
+++ my-folder/problems/two_sum/solution.cpp
@@ -1,8 +1,9 @@
+
 #include <vector>
 
 class Solution {
 public:
-    std::vector<int> twoSum(vector<int>& nums, int target) {
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
         for(int i = 0; i < nums.size() - 1; ++i){
             for(int j = i + 1; j < nums.size(); ++j){
                 if(nums[i] + nums[j] == target){
