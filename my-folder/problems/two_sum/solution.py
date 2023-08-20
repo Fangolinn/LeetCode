200f26bf9c1d998df310769993ed40fb7a7f2322
Fangolin <jakubkowalczyk0192@o2.pl>
2023-08-21 00:04:01 +0200

    Remove folder

--- my-folder/problems/two_sum/solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i, x in enumerate(nums[:-1]):
-            for j, y in enumerate(nums[i + 1:]):
-                if x + y == target:
-                    return [i, i + j + 1]
-        
