547dea666e7f44ee87ee36c6005ba269ddf50ec9
Jakub Kowalczyk <36115121+Fangolinn@users.noreply.github.com>
2023-11-06 19:28:57 +0000

    Sync LeetCode submission - Sort Colors (python3)

---
+++ leetcode-sync/problems/sort_colors/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i: int = 0
+
+        for current_num in (0, 1):
+            j: int = len(nums) - 1
+
+            while i <= j:
+                if nums[i] == current_num:
+                    i += 1
+                    continue
+
+                if nums[j] == current_num:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+
+                j -=1
+
