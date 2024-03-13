49d538c1885600ad263afe046d3ad1f0c8435077
Jakub Kowalczyk <36115121+Fangolinn@users.noreply.github.com>
2024-03-13 12:23:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.2 MB (63.97%)

---
+++ leetcode-sync/0070-climbing-stairs/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        int prev = 1;
+        int current = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int tmp = current;
+            current = prev + current;
+            prev = tmp; 
+        }
+
+        return current;
+    }
+}
