1ea7b91c89bd3ccbc44306e9db2479c1d470528b
Jakub Kowalczyk <36115121+Fangolinn@users.noreply.github.com>
2024-03-21 09:57:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.4 MB (49.36%)

---
+++ leetcode-sync/0206-reverse-linked-list/solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // public class ListNode {
+    //     int val;
+    //     ListNode next;
+
+    //     ListNode() {
+    //     }
+
+    //     ListNode(int val) {
+    //         this.val = val;
+    //     }
+
+    //     ListNode(int val, ListNode next) {
+    //         this.val = val;
+    //         this.next = next;
+    //     }
+    // }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prevNode = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prevNode;
+            prevNode = head;
+            head = next;
+        }
+
+        return prevNode;
+    }
+}
