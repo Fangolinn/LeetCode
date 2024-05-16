fb0700c48a7d1755347950907d5d3a7f83358176
Jakub Kowalczyk <36115121+Fangolinn@users.noreply.github.com>
2024-05-16 07:53:24 +0000

    LeetSync Runtime - 0 ms (100.00%), Memory - 44.3 MB (21.27%)

---
+++ leetcode-sync/2416-evaluate-boolean-binary-tree/solution.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    // public class TreeNode {
+    //     int val;
+    //     TreeNode left;
+    //     TreeNode right;
+
+    //     TreeNode() {
+    //     }
+
+    //     TreeNode(int val) {
+    //         this.val = val;
+    //     }
+
+    //     TreeNode(int val, TreeNode left, TreeNode right) {
+    //         this.val = val;
+    //         this.left = left;
+    //         this.right = right;
+    //     }
+    // }
+
+    public boolean evaluateTree(TreeNode root) {
+        switch (root.val) {
+            case 2:
+                return evaluateTree(root.left) || evaluateTree(root.right);
+            case 3:
+                return evaluateTree(root.left) && evaluateTree(root.right);
+            default: // implicitly the node has to be a leaf and have value of 1 or 2
+                return root.val == 1;
+        }
+    }
+}
