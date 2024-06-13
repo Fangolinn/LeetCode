b85d39aabfefad5cd59525cc6b71df2f214ff294
Jakub Kowalczyk <36115121+Fangolinn@users.noreply.github.com>
2024-06-13 10:27:10 +0200

    Delete leetcode-sync/problems directory
    outdated solutions, solutions are no longer synced there

--- leetcode-sync/problems/concatenation_of_array/solution.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: list[int]) -> list[int]:
-        return nums * 2
-        
--- leetcode-sync/problems/kth_largest_element_in_an_array/solution.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def findKthLargest(self, nums: list[int], k: int) -> int:
-        return sorted(nums)[-k]
-        
